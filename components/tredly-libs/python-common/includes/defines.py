03552e8f63aea744cca4383b873e1c89b1e45426
Laurence Odgers <laurence@vuid.com>
2016-06-30 14:46:40 +1000

    Update /boot/loader.conf and /etc/sysctl.conf to handle postgres/sysvipc

--- components/tredly-libs/python-common/includes/defines.py
+++ components/tredly-libs/python-common/includes/defines.py
@@ -49,8 +49,8 @@ global VERSON_NUMBER
 global VERSION_DATE
 
 # set version/date
-VERSION_NUMBER = "1.0.2"
-VERSION_DATE = "June 27 2016"
+VERSION_NUMBER = "1.0.3"
+VERSION_DATE = "June 30 2016"
 
 # ZFS Dataset locations
 ZFS_ROOT = "zroot"
